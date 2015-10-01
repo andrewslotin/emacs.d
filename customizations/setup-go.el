34970711957c0bee2629b99837c9679a40ddc8aa
Andrew Slotin <andrew.slotin@gmail.com>
2015-10-01 12:17:45 +0200

    Replace goflycheck with goimports
    After upgrade to Go 1.5 compiling the source code became a real pain >_<

--- customizations/setup-go.el
+++ customizations/setup-go.el
@@ -2,16 +2,20 @@
 ;; Go
 ;;;;
 
-;; Load go-mode
-;; (require 'go-mode-load)
-
 (setq gopath "/Users/suddendef/Development/Go")
 
 ;; Set GOPATH
 (setenv "GOPATH" gopath)
 
 ;; Syntax check with goflymake
-(require 'go-flycheck)
+(eval-after-load "go-mode"
+  '(progn
+     (flycheck-define-checker go-gofmt
+                               "A Go syntax and style checker using the gofmt utility."
+                               :command ("goimports" "-e" source)
+                               :error-patterns ((error line-start (file-name) ":" line ":" column ": " (message) line-end))
+                               :modes 'go-mode)
+     (add-to-list 'flycheck-checkers 'go-gofmt)))
 
 (defun custom-go-mode-hook()
   ;; Run goimports on save
