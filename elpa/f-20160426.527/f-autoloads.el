9051c8eb7b8e84f604ac4b141bed0a8129dbbda4
Andrew Slotin <andrew.slotin@gmail.com>
2016-07-15 23:38:03 +0200

    Upgrade f

--- elpa/f-20160426.527/f-autoloads.el
+++ elpa/f-20160426.527/f-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("f.el") (22294 41214 0 0))
+;;;### (autoloads nil nil ("f.el") (22409 22440 0 0))
 
 ;;;***
 
