1f4e5d84dcdd221eb62d72c92a8ff2e2e4d7299e
Andrew Slotin <andrew.slotin@gmail.com>
2016-07-16 02:00:51 +0200

    Upgrade zenburn-theme

--- elpa/zenburn-theme-20160501.749/zenburn-theme-autoloads.el
+++ elpa/zenburn-theme-20160501.749/zenburn-theme-autoloads.el
@@ -1,9 +1,9 @@
 ;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22294 41200
+;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22409 30973
 ;;;;;;  0 0))
 ;;; Generated autoloads from zenburn-theme.el
 
---
+++ elpa/zenburn-theme-20160501.749/zenburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zenburn-theme" "20160501.749" "A low contrast color theme for Emacs." 'nil :stars '(#("504" 0 3 (font-lock-face paradox-star-face))) :url "http://github.com/bbatsov/zenburn-emacs")
