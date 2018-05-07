72c76f1b25749a754a65c7338dc8d0c1828750c0
Andrew Slotin <andrew.slotin@gmail.com>
2018-05-07 14:19:16 +0400

    Upgrade packages

--- elpa/rustfmt-20160217.542/rustfmt-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rustfmt" "20160217.542" "Format rust code using rustfmt" '((emacs "24")) :url "https://github.com/fbergroth/emacs-rustfmt" :keywords '("convenience"))
